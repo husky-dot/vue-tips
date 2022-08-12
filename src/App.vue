9f3886cfff01b29c3dc23aa51dd723bcfa05a6e9
wangzhiyi <35094245+qq449245884@users.noreply.github.com>
2022-08-12 13:46:16 +0800

    feat: 完成

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,20 @@
-<template>
-  <User :userInfo="userInfo" />
-  <button @click="updateUserName">改变姓名</button>
-</template>
-
 <script setup>
-import { reactive } from "vue";
-import User from "./components/User.vue";
 
-const userInfo = reactive({
-  detail: {
-    name: "王大冶",
-  },
-  job: "前端开发",
-  age: 16,
+// `toRef` 可以用来为源响应式对象的某个 `property` 新创建一个 `ref`，
+// 然后 `ref `可以被传递，它会保持对其源 `property` 的响应式连接。
+
+import { reactive, toRef } from "vue";
+
+const state = reactive({
+  foo: 1,
+  bar: 2,
 });
 
-const updateUserName = () => {
-  userInfo.detail.name = "小明";
-};
+const fooRef = toRef(state, "foo");
+
+fooRef.value++;
+console.log(state.foo); // 2
+
+state.foo++;
+console.log(fooRef.value); // 3
 </script>
